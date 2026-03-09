263c6deb01b8cc7af9fb27d7f6c0dfbfaa1a33e2
LeMinhTriet79 <trietleminh7979@gmail.com>
2026-03-09 21:29:37 +0700

    gần hoàn thành

--- online-learning-new/src/main/java/com/swd/online_learning/repository/UserRepository.java
+++ online-learning-new/src/main/java/com/swd/online_learning/repository/UserRepository.java
@@ -1,11 +1,18 @@
 package com.swd.online_learning.repository;
 
 import com.swd.online_learning.entity.User;
+import com.swd.online_learning.enums.RoleName;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
     Boolean existsByUsername(String username);
+
+    // Thêm các hàm này để phục vụ Admin
+    long countByRole_RoleName(RoleName roleName);
+    List<User> findByRole_RoleName(RoleName roleName);
+    boolean existsByEmail(String email);
 }
